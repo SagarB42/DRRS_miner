48ad2c48b9bf5568cf2c2bb8e2ab920c6ddf50dd
Rajdeep Virdy <z5498562@ad.unsw.edu.au>
2024-07-27 17:28:37 +1000

    Changed sha256d code so accelerating becomes easier.

--- project_files/sha256d/sha256d.cpp
+++ project_files/sha256d/sha256d.cpp
@@ -31,12 +31,120 @@ static const uint32_t K[] = {
 #define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
 #define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
 
-// SHA-256 transformation function
-void sha256_transform(uint32_t state[8], const uint8_t data[64]) {
+void sha256d(const uint8_t input[80], uint32_t output[8]) {
+    // SHA-256 initialization function
+    uint32_t state[8];
+    
+    state[0] = 0x6a09e667;
+    state[1] = 0xbb67ae85;
+    state[2] = 0x3c6ef372;
+    state[3] = 0xa54ff53a;
+    state[4] = 0x510e527f;
+    state[5] = 0x9b05688c;
+    state[6] = 0x1f83d9ab;
+    state[7] = 0x5be0cd19;
+
+    // SHA-256 prepare function for the first hash
+    uint8_t data1[128];
+    for (int i = 0; i < 80; i++) {
+        data1[i] = input[i];
+    }
+    data1[80] = 0x80;
+
+    for (int i = 81; i < 128; i++) {
+        data1[i] = 0x00;
+    }
+
+    uint64_t length = 640;
+    for (int i = 0; i < 8; i++) {
+        data1[127 - i] = (length >> (i * 8)) & 0xff;
+    }
+
+    uint8_t data[2][64];
+    for (int i = 0; i < 64; i++) {
+        data[0][i] = data1[i];
+    }
+    for (int i = 0; i < 64; i++) {
+        data[1][i] = data1[i + 64];
+    }
+
+    // SHA-256 transformation function
+    for (int t = 0; t < 2; t++) {
+        uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+
+        for (i = 0, j = 0; i < 16; ++i, j += 4)
+            m[i] = (data[t][j] << 24) | (data[t][j + 1] << 16) | (data[t][j + 2] << 8) | (data[t][j + 3]);
+        for (; i < 64; ++i)
+            m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+        a = state[0];
+        b = state[1];
+        c = state[2];
+        d = state[3];
+        e = state[4];
+        f = state[5];
+        g = state[6];
+        h = state[7];
+
+        for (i = 0; i < 64; ++i) {
+            t1 = h + EP1(e) + CH(e, f, g) + K[i] + m[i];
+            t2 = EP0(a) + MAJ(a, b, c);
+            h = g;
+            g = f;
+            f = e;
+            e = d + t1;
+            d = c;
+            c = b;
+            b = a;
+            a = t1 + t2;
+        }
+
+        state[0] += a;
+        state[1] += b;
+        state[2] += c;
+        state[3] += d;
+        state[4] += e;
+        state[5] += f;
+        state[6] += g;
+        state[7] += h;
+    }
+
+    uint32_t hash1[8];
+    for (int i = 0; i < 8; i++) {
+        hash1[i] = state[i];
+    }
+
+    // SHA-256 prepare function for the second hash
+    uint8_t data2[64];
+    for (int i = 0; i < 32; i++) {
+        data2[i] = (hash1[i / 4] >> (24 - 8 * (i % 4))) & 0xff;
+    }
+    data2[32] = 0x80;
+
+    for (int i = 33; i < 63; i++) {
+        data2[i] = 0x00;
+    }
+
+    length = 256;
+    for (int i = 0; i < 8; i++) {
+        data2[63 - i] = (length >> (i * 8)) & 0xff;
+    }
+
+    // SHA-256 initialization function
+    state[0] = 0x6a09e667;
+    state[1] = 0xbb67ae85;
+    state[2] = 0x3c6ef372;
+    state[3] = 0xa54ff53a;
+    state[4] = 0x510e527f;
+    state[5] = 0x9b05688c;
+    state[6] = 0x1f83d9ab;
+    state[7] = 0x5be0cd19;
+
+    // SHA-256 transformation function for the second hash
     uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
     for (i = 0, j = 0; i < 16; ++i, j += 4)
-        m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+        m[i] = (data2[j] << 24) | (data2[j + 1] << 16) | (data2[j + 2] << 8) | (data2[j + 3]);
     for (; i < 64; ++i)
         m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
@@ -70,102 +178,8 @@ void sha256_transform(uint32_t state[8], const uint8_t data[64]) {
     state[5] += f;
     state[6] += g;
     state[7] += h;
-}
-
-// SHA-256 initialization function
-void sha256_init(uint32_t state[8]) {
-    state[0] = 0x6a09e667;
-    state[1] = 0xbb67ae85;
-    state[2] = 0x3c6ef372;
-    state[3] = 0xa54ff53a;
-    state[4] = 0x510e527f;
-    state[5] = 0x9b05688c;
-    state[6] = 0x1f83d9ab;
-    state[7] = 0x5be0cd19;
-}
-
-// SHA-256 prepare function
-void sha256_prepare_1(const uint8_t input[80], uint8_t data[2][64]) {
-    //append the bit '1' to the message
-    uint8_t data1[128];
-    for (int i = 0; i < 80; i++) {
-        data1[i] = input[i];
-    }
-    data1[80] = 0x80;
-
-    //319 bit  '0'
-    for (int i = 81; i < 128; i++) {
-        data1[i] = 0x00;
-    }
-
-    //append the original length
-    uint64_t length = 640;
-    for (int i = 0; i < 8; i++) {
-        data1[127 - i] = (length >> (i * 8)) & 0xff;
-    }
-
-    //prepare the data
-    for (int i = 0; i < 64; i++) {
-        data[0][i] = data1[i];
-    }
-    for (int i = 0; i < 64; i++) {
-        data[1][i] = data1[i + 64];
-    }
-}
-
-// SHA256D function
-void sha256_1(const uint8_t input[80], uint32_t output[8]) {
-    uint32_t hash1[8];
-    sha256_init(hash1);
-    uint8_t data[2][64];
-    sha256_prepare_1(input, data);
-    sha256_transform(hash1, data[0]);
-    sha256_transform(hash1, data[1]);
 
     for (int i = 0; i < 8; i++) {
-        output[i] = hash1[i];
+        output[i] = state[i];
     }
-}
-
-// SHA256D prepare function
-void sha256_prepare_2(const uint32_t input[8], uint8_t data[64]) {
-    //append the bit '1' to the message
-    uint8_t data1[64];
-    for (int i = 0; i < 32; i++) {
-        data1[i] = (input[i / 4] >> (24 - 8 * (i % 4))) & 0xff;
-    }
-    data1[32] = 0x80;
-
-    // add 192 zero bits
-    for (int i = 33; i < 63; i++) {
-        data1[i] = 0x00;
-    }
-
-    //append the original length
-    uint64_t length = 256;
-    for (int i = 0; i < 8; i++) {
-        data1[63 - i] = (length >> (i * 8)) & 0xff;
-    }
-    //prepare the data
-    for (int i = 0; i < 64; i++) {
-        data[i] = data1[i];
-    }
-}
-
-void sha256_2(const uint32_t input[8], uint32_t output[8]) {
-    uint32_t hash1[8];
-    sha256_init(hash1);
-    uint8_t data[64];
-    sha256_prepare_2(input, data);
-    sha256_transform(hash1, data);
-
-    for (int i = 0; i < 8; i++) {
-        output[i] = hash1[i];
-    }
-}
-
-void sha256d(const uint8_t input[80], uint32_t output[8]) {
-    uint32_t hash1[8];
-    sha256_1(input, hash1);
-    sha256_2(hash1, output);
 }
