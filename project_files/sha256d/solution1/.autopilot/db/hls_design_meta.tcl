489c8bd0e649bf27cee186e0679e030f086e24b5
KnightRajSinVir <rajsinvir@gmail.com>
2024-07-27 11:07:19 +1000

    I forgot to stage my commits.

---
+++ project_files/sha256d/solution1/.autopilot/db/hls_design_meta.tcl
@@ -0,0 +1,2 @@
+set design_latency 1872
+set design_II 1872
---
+++ project_files/sha256d/solution1/.autopilot/db/sha256d.tbgen.tcl
@@ -0,0 +1,236 @@
+set moduleName sha256d
+set isTopModule 1
+set isTaskLevelControl 1
+set isCombinational 0
+set isDatapathOnly 0
+set isFreeRunPipelineModule 0
+set isPipelined 0
+set pipeline_type none
+set FunctionProtocol ap_ctrl_hs
+set isOneStateSeq 0
+set ProfileFlag 0
+set StallSigGenFlag 0
+set isEnableWaveformDebug 1
+set C_modelName {sha256d}
+set C_modelType { void 0 }
+set C_modelArgList {
+	{ input_V int 8 regular {array 80 { 1 3 } 1 1 }  }
+	{ output_V int 8 regular {array 32 { 0 0 } 0 1 }  }
+}
+set C_modelArgMapList {[ 
+	{ "Name" : "input_V", "interface" : "memory", "bitwidth" : 8, "direction" : "READONLY", "bitSlice":[{"low":0,"up":7,"cElement": [{"cName": "input.V","cData": "uint8","bit_use": { "low": 0,"up": 7},"cArray": [{"low" : 0,"up" : 79,"step" : 1}]}]}]} , 
+ 	{ "Name" : "output_V", "interface" : "memory", "bitwidth" : 8, "direction" : "WRITEONLY", "bitSlice":[{"low":0,"up":7,"cElement": [{"cName": "output.V","cData": "uint8","bit_use": { "low": 0,"up": 7},"cArray": [{"low" : 0,"up" : 31,"step" : 1}]}]}]} ]}
+# RTL Port declarations: 
+set portNum 17
+set portList { 
+	{ ap_clk sc_in sc_logic 1 clock -1 } 
+	{ ap_rst sc_in sc_logic 1 reset -1 active_high_sync } 
+	{ ap_start sc_in sc_logic 1 start -1 } 
+	{ ap_done sc_out sc_logic 1 predone -1 } 
+	{ ap_idle sc_out sc_logic 1 done -1 } 
+	{ ap_ready sc_out sc_logic 1 ready -1 } 
+	{ input_V_address0 sc_out sc_lv 7 signal 0 } 
+	{ input_V_ce0 sc_out sc_logic 1 signal 0 } 
+	{ input_V_q0 sc_in sc_lv 8 signal 0 } 
+	{ output_V_address0 sc_out sc_lv 5 signal 1 } 
+	{ output_V_ce0 sc_out sc_logic 1 signal 1 } 
+	{ output_V_we0 sc_out sc_logic 1 signal 1 } 
+	{ output_V_d0 sc_out sc_lv 8 signal 1 } 
+	{ output_V_address1 sc_out sc_lv 5 signal 1 } 
+	{ output_V_ce1 sc_out sc_logic 1 signal 1 } 
+	{ output_V_we1 sc_out sc_logic 1 signal 1 } 
+	{ output_V_d1 sc_out sc_lv 8 signal 1 } 
+}
+set NewPortList {[ 
+	{ "name": "ap_clk", "direction": "in", "datatype": "sc_logic", "bitwidth":1, "type": "clock", "bundle":{"name": "ap_clk", "role": "default" }} , 
+ 	{ "name": "ap_rst", "direction": "in", "datatype": "sc_logic", "bitwidth":1, "type": "reset", "bundle":{"name": "ap_rst", "role": "default" }} , 
+ 	{ "name": "ap_start", "direction": "in", "datatype": "sc_logic", "bitwidth":1, "type": "start", "bundle":{"name": "ap_start", "role": "default" }} , 
+ 	{ "name": "ap_done", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "predone", "bundle":{"name": "ap_done", "role": "default" }} , 
+ 	{ "name": "ap_idle", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "done", "bundle":{"name": "ap_idle", "role": "default" }} , 
+ 	{ "name": "ap_ready", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "ready", "bundle":{"name": "ap_ready", "role": "default" }} , 
+ 	{ "name": "input_V_address0", "direction": "out", "datatype": "sc_lv", "bitwidth":7, "type": "signal", "bundle":{"name": "input_V", "role": "address0" }} , 
+ 	{ "name": "input_V_ce0", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "signal", "bundle":{"name": "input_V", "role": "ce0" }} , 
+ 	{ "name": "input_V_q0", "direction": "in", "datatype": "sc_lv", "bitwidth":8, "type": "signal", "bundle":{"name": "input_V", "role": "q0" }} , 
+ 	{ "name": "output_V_address0", "direction": "out", "datatype": "sc_lv", "bitwidth":5, "type": "signal", "bundle":{"name": "output_V", "role": "address0" }} , 
+ 	{ "name": "output_V_ce0", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "signal", "bundle":{"name": "output_V", "role": "ce0" }} , 
+ 	{ "name": "output_V_we0", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "signal", "bundle":{"name": "output_V", "role": "we0" }} , 
+ 	{ "name": "output_V_d0", "direction": "out", "datatype": "sc_lv", "bitwidth":8, "type": "signal", "bundle":{"name": "output_V", "role": "d0" }} , 
+ 	{ "name": "output_V_address1", "direction": "out", "datatype": "sc_lv", "bitwidth":5, "type": "signal", "bundle":{"name": "output_V", "role": "address1" }} , 
+ 	{ "name": "output_V_ce1", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "signal", "bundle":{"name": "output_V", "role": "ce1" }} , 
+ 	{ "name": "output_V_we1", "direction": "out", "datatype": "sc_logic", "bitwidth":1, "type": "signal", "bundle":{"name": "output_V", "role": "we1" }} , 
+ 	{ "name": "output_V_d1", "direction": "out", "datatype": "sc_lv", "bitwidth":8, "type": "signal", "bundle":{"name": "output_V", "role": "d1" }}  ]}
+
+set RtlHierarchyInfo {[
+	{"ID" : "0", "Level" : "0", "Path" : "`AUTOTB_DUT_INST", "Parent" : "", "Child" : ["1", "2", "3", "4", "9"],
+		"CDFG" : "sha256d",
+		"Protocol" : "ap_ctrl_hs",
+		"ControlExist" : "1", "ap_start" : "1", "ap_ready" : "1", "ap_done" : "1", "ap_continue" : "0", "ap_idle" : "1",
+		"Pipeline" : "None", "UnalignedPipeline" : "0", "RewindPipeline" : "0", "ProcessNetwork" : "0",
+		"II" : "0",
+		"VariableLatency" : "1", "ExactLatency" : "-1", "EstimateLatencyMin" : "1872", "EstimateLatencyMax" : "1872",
+		"Combinational" : "0",
+		"Datapath" : "0",
+		"ClockEnable" : "0",
+		"HasSubDataflow" : "0",
+		"InDataflowNetwork" : "0",
+		"HasNonBlockingOperation" : "0",
+		"WaitState" : [
+			{"State" : "ap_ST_fsm_state6", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_update_fu_372"},
+			{"State" : "ap_ST_fsm_state14", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_update_1_fu_381"}],
+		"Port" : [
+			{"Name" : "input_V", "Type" : "Memory", "Direction" : "I",
+				"SubConnect" : [
+					{"ID" : "4", "SubInstance" : "grp_sha256_update_fu_372", "Port" : "data_V"}]},
+			{"Name" : "output_V", "Type" : "Memory", "Direction" : "O"},
+			{"Name" : "K_V", "Type" : "Memory", "Direction" : "I",
+				"SubConnect" : [
+					{"ID" : "9", "SubInstance" : "grp_sha256_update_1_fu_381", "Port" : "K_V"},
+					{"ID" : "4", "SubInstance" : "grp_sha256_update_fu_372", "Port" : "K_V"}]}]},
+	{"ID" : "1", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.state_V_1_U", "Parent" : "0"},
+	{"ID" : "2", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.state_V_U", "Parent" : "0"},
+	{"ID" : "3", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.hash1_V_U", "Parent" : "0"},
+	{"ID" : "4", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_fu_372", "Parent" : "0", "Child" : ["5", "6"],
+		"CDFG" : "sha256_update",
+		"Protocol" : "ap_ctrl_hs",
+		"ControlExist" : "1", "ap_start" : "1", "ap_ready" : "1", "ap_done" : "1", "ap_continue" : "0", "ap_idle" : "1",
+		"Pipeline" : "None", "UnalignedPipeline" : "0", "RewindPipeline" : "0", "ProcessNetwork" : "0",
+		"II" : "0",
+		"VariableLatency" : "1", "ExactLatency" : "-1", "EstimateLatencyMin" : "906", "EstimateLatencyMax" : "906",
+		"Combinational" : "0",
+		"Datapath" : "0",
+		"ClockEnable" : "0",
+		"HasSubDataflow" : "0",
+		"InDataflowNetwork" : "0",
+		"HasNonBlockingOperation" : "0",
+		"WaitState" : [
+			{"State" : "ap_ST_fsm_state4", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_transform_fu_186"},
+			{"State" : "ap_ST_fsm_state10", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_transform_fu_186"}],
+		"Port" : [
+			{"Name" : "state_V", "Type" : "Memory", "Direction" : "IO",
+				"SubConnect" : [
+					{"ID" : "6", "SubInstance" : "grp_sha256_transform_fu_186", "Port" : "state_V"}]},
+			{"Name" : "data_V", "Type" : "Memory", "Direction" : "I"},
+			{"Name" : "K_V", "Type" : "Memory", "Direction" : "I",
+				"SubConnect" : [
+					{"ID" : "6", "SubInstance" : "grp_sha256_transform_fu_186", "Port" : "K_V"}]}]},
+	{"ID" : "5", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_fu_372.block_V_U", "Parent" : "4"},
+	{"ID" : "6", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_fu_372.grp_sha256_transform_fu_186", "Parent" : "4", "Child" : ["7", "8"],
+		"CDFG" : "sha256_transform",
+		"Protocol" : "ap_ctrl_hs",
+		"ControlExist" : "1", "ap_start" : "1", "ap_ready" : "1", "ap_done" : "1", "ap_continue" : "0", "ap_idle" : "1",
+		"Pipeline" : "None", "UnalignedPipeline" : "0", "RewindPipeline" : "0", "ProcessNetwork" : "0",
+		"II" : "0",
+		"VariableLatency" : "1", "ExactLatency" : "-1", "EstimateLatencyMin" : "314", "EstimateLatencyMax" : "314",
+		"Combinational" : "0",
+		"Datapath" : "0",
+		"ClockEnable" : "0",
+		"HasSubDataflow" : "0",
+		"InDataflowNetwork" : "0",
+		"HasNonBlockingOperation" : "0",
+		"Port" : [
+			{"Name" : "state_V", "Type" : "Memory", "Direction" : "IO"},
+			{"Name" : "data_V", "Type" : "Memory", "Direction" : "I"},
+			{"Name" : "K_V", "Type" : "Memory", "Direction" : "I"}]},
+	{"ID" : "7", "Level" : "3", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_fu_372.grp_sha256_transform_fu_186.K_V_U", "Parent" : "6"},
+	{"ID" : "8", "Level" : "3", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_fu_372.grp_sha256_transform_fu_186.m_V_U", "Parent" : "6"},
+	{"ID" : "9", "Level" : "1", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_1_fu_381", "Parent" : "0", "Child" : ["10", "11"],
+		"CDFG" : "sha256_update_1",
+		"Protocol" : "ap_ctrl_hs",
+		"ControlExist" : "1", "ap_start" : "1", "ap_ready" : "1", "ap_done" : "1", "ap_continue" : "0", "ap_idle" : "1",
+		"Pipeline" : "None", "UnalignedPipeline" : "0", "RewindPipeline" : "0", "ProcessNetwork" : "0",
+		"II" : "0",
+		"VariableLatency" : "1", "ExactLatency" : "-1", "EstimateLatencyMin" : "906", "EstimateLatencyMax" : "906",
+		"Combinational" : "0",
+		"Datapath" : "0",
+		"ClockEnable" : "0",
+		"HasSubDataflow" : "0",
+		"InDataflowNetwork" : "0",
+		"HasNonBlockingOperation" : "0",
+		"WaitState" : [
+			{"State" : "ap_ST_fsm_state4", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_transform_fu_186"},
+			{"State" : "ap_ST_fsm_state10", "FSM" : "ap_CS_fsm", "SubInstance" : "grp_sha256_transform_fu_186"}],
+		"Port" : [
+			{"Name" : "state_V", "Type" : "Memory", "Direction" : "IO",
+				"SubConnect" : [
+					{"ID" : "11", "SubInstance" : "grp_sha256_transform_fu_186", "Port" : "state_V"}]},
+			{"Name" : "data_V", "Type" : "Memory", "Direction" : "I"},
+			{"Name" : "K_V", "Type" : "Memory", "Direction" : "I",
+				"SubConnect" : [
+					{"ID" : "11", "SubInstance" : "grp_sha256_transform_fu_186", "Port" : "K_V"}]}]},
+	{"ID" : "10", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_1_fu_381.block_V_U", "Parent" : "9"},
+	{"ID" : "11", "Level" : "2", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_1_fu_381.grp_sha256_transform_fu_186", "Parent" : "9", "Child" : ["12", "13"],
+		"CDFG" : "sha256_transform",
+		"Protocol" : "ap_ctrl_hs",
+		"ControlExist" : "1", "ap_start" : "1", "ap_ready" : "1", "ap_done" : "1", "ap_continue" : "0", "ap_idle" : "1",
+		"Pipeline" : "None", "UnalignedPipeline" : "0", "RewindPipeline" : "0", "ProcessNetwork" : "0",
+		"II" : "0",
+		"VariableLatency" : "1", "ExactLatency" : "-1", "EstimateLatencyMin" : "314", "EstimateLatencyMax" : "314",
+		"Combinational" : "0",
+		"Datapath" : "0",
+		"ClockEnable" : "0",
+		"HasSubDataflow" : "0",
+		"InDataflowNetwork" : "0",
+		"HasNonBlockingOperation" : "0",
+		"Port" : [
+			{"Name" : "state_V", "Type" : "Memory", "Direction" : "IO"},
+			{"Name" : "data_V", "Type" : "Memory", "Direction" : "I"},
+			{"Name" : "K_V", "Type" : "Memory", "Direction" : "I"}]},
+	{"ID" : "12", "Level" : "3", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_1_fu_381.grp_sha256_transform_fu_186.K_V_U", "Parent" : "11"},
+	{"ID" : "13", "Level" : "3", "Path" : "`AUTOTB_DUT_INST.grp_sha256_update_1_fu_381.grp_sha256_transform_fu_186.m_V_U", "Parent" : "11"}]}
+
+
+set ArgLastReadFirstWriteLatency {
+	sha256d {
+		input_V {Type I LastRead 4 FirstWrite -1}
+		output_V {Type O LastRead -1 FirstWrite 13}
+		K_V {Type I LastRead -1 FirstWrite -1}}
+	sha256_update {
+		state_V {Type IO LastRead 6 FirstWrite 7}
+		data_V {Type I LastRead 4 FirstWrite -1}
+		K_V {Type I LastRead -1 FirstWrite -1}}
+	sha256_transform {
+		state_V {Type IO LastRead 6 FirstWrite 7}
+		data_V {Type I LastRead 1 FirstWrite -1}
+		K_V {Type I LastRead -1 FirstWrite -1}}
+	sha256_update_1 {
+		state_V {Type IO LastRead 6 FirstWrite 7}
+		data_V {Type I LastRead 4 FirstWrite -1}
+		K_V {Type I LastRead -1 FirstWrite -1}}
+	sha256_transform {
+		state_V {Type IO LastRead 6 FirstWrite 7}
+		data_V {Type I LastRead 1 FirstWrite -1}
+		K_V {Type I LastRead -1 FirstWrite -1}}}
+
+set hasDtUnsupportedChannel 0
+
+set PerformanceInfo {[
+	{"Name" : "Latency", "Min" : "1872", "Max" : "1872"}
+	, {"Name" : "Interval", "Min" : "1873", "Max" : "1873"}
+]}
+
+set PipelineEnableSignalInfo {[
+]}
+
+set Spec2ImplPortList { 
+	input_V { ap_memory {  { input_V_address0 mem_address 1 7 }  { input_V_ce0 mem_ce 1 1 }  { input_V_q0 mem_dout 0 8 } } }
+	output_V { ap_memory {  { output_V_address0 mem_address 1 5 }  { output_V_ce0 mem_ce 1 1 }  { output_V_we0 mem_we 1 1 }  { output_V_d0 mem_din 1 8 }  { output_V_address1 MemPortADDR2 1 5 }  { output_V_ce1 MemPortCE2 1 1 }  { output_V_we1 MemPortWE2 1 1 }  { output_V_d1 MemPortDIN2 1 8 } } }
+}
+
+set busDeadlockParameterList { 
+}
+
+# RTL port scheduling information:
+set fifoSchedulingInfoList { 
+}
+
+# RTL bus port read request latency information:
+set busReadReqLatencyList { 
+}
+
+# RTL bus port write response latency information:
+set busWriteResLatencyList { 
+}
+
+# RTL array port load latency information:
+set memoryLoadLatencyList { 
+}
